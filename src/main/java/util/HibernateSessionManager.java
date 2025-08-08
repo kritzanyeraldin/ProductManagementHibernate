4aa74f2e0256dc77530b02415b84ecc3078d7907
kritzanyeraldin <kritzan.sullca.e@uni.pe>
2025-08-07 19:19:40 -0500

    docs: update Readme

--- src/main/java/util/HibernateSessionManager.java
+++ src/main/java/util/HibernateSessionManager.java
@@ -4,7 +4,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.sql.*;
 
 public class HibernateSessionManager {
     private static final SessionFactory sessionFactory = buildSessionFactory();
